f6e0c714d073acf191f363f0f68ac8675f7d06cf
Shikharcodes13 <144451664+Shikharcodes13@users.noreply.github.com>
2024-07-13 21:42:01 +0530

    Add files via upload

---
+++ Day_01.js
@@ -0,0 +1,20 @@
+var Num= 1234
+let Name= "Shikhar"
+const bool=true
+const citizen=India
+console.log(Num)
+console.log(Name)
+console.log(bool)
+
+let Email=null;
+console.log(typeof Num)
+console.log(typeof Name)
+console.log(typeof bool)
+console.log(typeof Email)
+
+let Roll= 9019011721
+console.log(Roll)
+Roll= 9919011721
+console.log(Roll)
+
+citizen= Usa // not applicable for const
