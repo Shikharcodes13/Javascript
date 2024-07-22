7c77450d8027e25831908226ada6b7bfc4ecfc6a
Shikharcodes13 <144451664+Shikharcodes13@users.noreply.github.com>
2024-07-22 17:30:51 +0530

    Add files via upload

---
+++ Day_02.js
@@ -0,0 +1,36 @@
+// Operators
+let a =24;
+let b=29;
+
+let d=43;
+let c="43";
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+a+=10;
+console.log(a);
+
+b-=10;
+console.log(b);
+
+// Programs
+if(a>b){
+    console.log("a is greater than b")
+}
+else{
+    console.log("b is greater than a");
+}
+
+console.log(c==d)
+console.log(c===d)
+
+console.log( a>b && c>d);
+console.log(b>a && c>d);
+
+console.log(!(c===d));
+
